68ba3b5cf68942a433e370ae96c9a078462e98a2
Julien Cabieces <julien.cabieces@oslandia.com>
2022-08-31 17:57:58 +0200

    put everything in the script file

--- tests/code_layout/test_clangtidy.sh
+++ tests/code_layout/test_clangtidy.sh
@@ -2,5 +2,14 @@
 
 for i in $@; do echo test1: $i; done
 
-exit 0
-clang-tidy -p=build -checks="bugprone-*,-bugprone-easily-swappable-parameters,-bugprone-narrowing-conversions,-bugprone-virtual-near-miss" $@
+files=$@
+for i in ${files[@]}; do echo test1: $i; done
+
+for file in ${files[@]}; do [[ $file =~ .*\.(cpp|h)$ ]] && filtered+=("$file"); done
+for i in ${filtered[@]}; do echo test1: $i; done
+
+/root/QGIS/.docker/docker-qgis-configure.sh
+
+cd /root/QGIS/
+
+clang-tidy -p=build -checks="bugprone-*,-bugprone-easily-swappable-parameters,-bugprone-narrowing-conversions,-bugprone-virtual-near-miss" ${filtered[@]}
