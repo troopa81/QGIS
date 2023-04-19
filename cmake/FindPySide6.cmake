6316107f1431e4b8de4ec1d5da0cc3b1d4f5572d
Julien Cabieces <julien.cabieces@oslandia.com>
2023-04-19 08:54:18 +0200

    add QTXml PySide module

--- cmake/FindPySide6.cmake
+++ cmake/FindPySide6.cmake
@@ -123,6 +123,7 @@ if(PYSIDE6_FOUND)
         ${PYSIDE_INCLUDE_DIR}/QtCore/
         ${PYSIDE_INCLUDE_DIR}/QtGui/
         ${PYSIDE_INCLUDE_DIR}/QtWidgets/
+        ${PYSIDE_INCLUDE_DIR}/QtXml/
         ${Python3_INCLUDE_DIRS}
     )
 endif()
