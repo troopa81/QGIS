b18fc0543e0096f1e8f88a6d146b5b70188f8769
Julien Cabieces <julien.cabieces@oslandia.com>
2023-04-20 15:26:08 +0200

    add PySide 6 mock folder

---
+++ qt-for-python/qgis/PyQt/PyQt6/uic/pyuic.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+"""
+***************************************************************************
+    pyuic.py
+    ---------------------
+    Date                 : March 2016
+    Copyright            : (C) 2016 by Juergen E. Fischer
+    Email                : jef at norbit dot de
+***************************************************************************
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+***************************************************************************
+"""
+
+__author__ = 'Denis Rouzaud'
+__date__ = 'May 2021'
+__copyright__ = '(C) 2021, Denis Rouzaud'
+
+from PySide2.uic import pyuic
+if (callable(pyuic.main)):
+    pyuic.main()
