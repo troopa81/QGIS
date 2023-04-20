0ce21448120548a92a09a13621e81acd36be12cd
Julien Cabieces <julien.cabieces@oslandia.com>
2023-04-20 15:26:08 +0200

    add PySide 6 mock folder

---
+++ qt-for-python/qgis/PyQt/PyQt6/Qt.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+"""
+***************************************************************************
+    Qt.py
+    ---------------------
+    Date                 : September 2016
+    Copyright            : (C) 2015 by Marco Bernasocchi
+    Email                : marco at opengis dot ch
+***************************************************************************
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+***************************************************************************
+"""
+
+__author__ = 'Denis Rouzaud'
+__date__ = 'May 2021'
+__copyright__ = '(C) 2021, Denis Rouzaud'
+
+
+from PySide6.Qt import *
---
+++ qt-for-python/qgis/PyQt/PyQt6/QtPrintSupport.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+"""
+***************************************************************************
+    QtPrintSupport.py
+    ---------------------
+    Date                 : November 2015
+    Copyright            : (C) 2015 by Matthias Kuhn
+    Email                : matthias at opengis dot ch
+***************************************************************************
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+***************************************************************************
+"""
+
+__author__ = 'Denis Rouzaud'
+__date__ = 'May 2021'
+__copyright__ = '(C) 2021, Denis Rouzaud'
+
+from PySide6.QtPrintSupport import *
