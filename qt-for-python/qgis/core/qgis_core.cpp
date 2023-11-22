fc58f11a319a28669072f3d6196d224b7df8de5e
Julien Cabieces <julien.cabieces@oslandia.com>
2023-11-22 10:37:29 +0100

    add missing file

---
+++ qt-for-python/qgis/core/qgis_core.cpp
@@ -0,0 +1,16 @@
+
+// @snippet QgsFeature-__setitem__
+QVariant cppValue = % CONVERTTOCPP[QVariant]( _value );
+
+qDebug() << "cppValue=" << cppValue;
+
+bool rv = % CPPSELF->setAttribute( _i, cppValue );
+
+if ( !rv )
+{
+  PyErr_SetString( PyExc_KeyError, QByteArray::number( _i ) );
+  return -1;
+}
+
+return 0;
+// @snippet QgsFeature-__setitem__
