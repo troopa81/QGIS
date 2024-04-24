748fa878c8d9f07467a7d0b46d7b8efded87cad0
Julien Cabieces <julien.cabieces@oslandia.com>
2024-04-24 08:51:30 +0200

    make python API backward compatible

--- src/core/expression/qgsexpression.cpp
+++ src/core/expression/qgsexpression.cpp
@@ -125,6 +125,11 @@ QString QgsExpression::quotedValue( const QVariant &value, QMetaType::Type type 
 
 }
 
+QString QgsExpression::quotedValue( const QVariant &value, QVariant::Type type )
+{
+  return quotedValue( value, QgsVariantUtils::variantTypeToMetaType( type ) );
+}
+
 bool QgsExpression::isFunctionName( const QString &name )
 {
   return functionIndex( name ) != -1;
@@ -1160,6 +1165,11 @@ QString QgsExpression::createFieldEqualityExpression( const QString &fieldName, 
   return expr;
 }
 
+QString QgsExpression::createFieldEqualityExpression( const QString &fieldName, const QVariant &value, QVariant::Type fieldType )
+{
+  return createFieldEqualityExpression( fieldName, value, QgsVariantUtils::variantTypeToMetaType( fieldType ) );
+}
+
 bool QgsExpression::isFieldEqualityExpression( const QString &expression, QString &field, QVariant &value )
 {
   QgsExpression e( expression );
@@ -1429,6 +1439,3 @@ QList<const QgsExpressionNode *> QgsExpression::nodes() const
 
   return d->mRootNode->nodes();
 }
-
-
-
